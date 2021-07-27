ba6d4eeffce8bd931f081afb604454dd40c57aea
Tobias Tefke <tobias@tefke.name>
2021-07-27 18:07:38 +0200

    Remove report section for now

--- website/share/index.php
+++ website/share/index.php
@@ -38,8 +38,7 @@ session_start();
   <div class="container">
     <div class="row">
       <div class="col-sm-6">
-        <div class="card" style="width: 18 rem;">
-          <img src="..." class="card-img-top" alt="...">
+        <div class="card" style="width: 18 rem; height: 13rem;">
           <div class="card-body">
             <h5 class="card-title">Room Management</h5>
             <p class="card-text">Here, you can adjust the room settings. Furthermore, you can restrict the access to the room</p>
@@ -48,27 +47,13 @@ session_start();
         </div>
       </div>
       <div class="col-sm-6">
-        <div class="card" style="width: 18 rem;">
-          <img src="..." class="card-img-top" alt="...">
+        <div class="card" style="width: 18 rem; height: 13rem;">
           <div class="card-body">
             <h5 class="card-title">User Management</h5>
-            <p class="card-text">Here, you can manage the user accounts. You can also set user tags here</p>
+            <p class="card-text">Here, you can manage the user accounts. You can also set user tags here and ban users or lift the bans.</p>
             <a href="user.php" class="btn btn-primary">User Management</a>
           </div>
         </div>
       </div>
-        <div class="col-sm-6">
-          <div class="card" style="width: 18 rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Manage Reports</h5>
-              <p class="card-text">Here, you can see, which users have been reported. Furthermore, you can view the reports details and impose sanctions on non-behaving users</p>
-              <a href="#" class="btn btn-primary">Reports Management</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </body>
 </html>
