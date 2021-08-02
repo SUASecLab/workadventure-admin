5ece2f2a6952de80f84b720a59bf78f434f170ef
Tobias Tefke <tobias@tefke.name>
2021-08-02 15:10:43 +0200

    Update table definitions

--- db/firstrun/init.sql
+++ db/firstrun/init.sql
@@ -1,39 +1,43 @@
 CREATE TABLE users (
   uuid varchar(36) NOT NULL,
-  name varchar(30),
-  email varchar(30),
+  name varchar(30) NOT NULL,
+  email varchar(30) NOT NULL,
   PRIMARY KEY (uuid)
 );
 
 CREATE TABLE tags (
   uuid varchar(36) NOT NULL,
   tag varchar(15) NOT NULL,
-  PRIMARY KEY (uuid, tag)
+  PRIMARY KEY (uuid, tag),
+  FOREIGN KEY (uuid) REFERENCES users(uuid)
 );
 
 CREATE TABLE reports (
-    report_id integer NOT NULL AUTO_INCREMENT,
-    reportedUserUuid varchar(36) NOT NULL,
-    reportedUserComment varchar(1000) NOT NULL,
-    reporterUserUuid varchar(36) NOT NULL,
-    reportWorldSlug varchar(20) NOT NULL,
-    PRIMARY KEY (report_id)
+  report_id integer NOT NULL AUTO_INCREMENT,
+  reportedUserUuid varchar(36) NOT NULL,
+  reportedUserComment varchar(1000) NOT NULL,
+  reporterUserUuid varchar(36) NOT NULL,
+  reportWorldSlug varchar(20) NOT NULL,
+  PRIMARY KEY (report_id),
+  FOREIGN KEY (reportedUserUuid) REFERENCES users(uuid),
+  FOREIGN KEY (reporterUserUuid) REFERENCES users(uuid)
 );
 
 CREATE TABLE banned_users (
-    uuid varchar(36) NOT NULL,
-    ban_message varchar(50) NOT NULL,
-    PRIMARY KEY (uuid)
+  uuid varchar(36) NOT NULL,
+  ban_message varchar(50) NOT NULL,
+  PRIMARY KEY (uuid),
+  FOREIGN KEY (uuid) REFERENCES users(uuid)
 );
 
 CREATE TABLE website (
-    username varchar(16) NOT NULL,
-    hashed_password varchar(160) NOT NULL,
-    PRIMARY KEY (username)
+  username varchar(16) NOT NULL,
+  hashed_password varchar(160) NOT NULL,
+  PRIMARY KEY (username)
 );
 
 CREATE TABLE maps (
-    map_url varchar(100) NOT NULL,
-    map_file_url varchar(100) NOT NULL,
-    PRIMARY KEY (map_url)
+  map_url varchar(100) NOT NULL,
+  map_file_url varchar(100) NOT NULL,
+  PRIMARY KEY (map_url)
 );
