c0aa41a31d59de9925951e7c48b1e650745bde2a
Tobias Tefke <tobias@tefke.name>
2021-12-27 15:45:05 +0100

    Improve responsiveness of map settings

--- website_src/js/ajax/rooms.js
+++ website_src/js/ajax/rooms.js
@@ -2,160 +2,17 @@ var tagsElementsShown = false;
 var newMapsTags = [];
 
 window.onload = function () {
-    loadMaps();
-
-    document.getElementById("radioPublic").addEventListener("click", removeTagsElements);
-    document.getElementById("radioMembers").addEventListener("click", removeTagsElements);
-    document.getElementById("radioMembersTags").addEventListener("click", addTagsElements);
-    document.getElementById("addMapButton").addEventListener("click", addMap);
+    load();
 }
 
-function loadMaps() {
-    $("#maps").hide();
-    $.get("ajax_rooms.php", function (data, status) {
-        var contentBox = document.getElementById("maps");
-        contentBox.innerHTML = "";
-
-        var addMapParagraph = document.getElementById("add-map-paragraph");
-        var mapUrlInput = document.getElementById("mapURL");
-
-        if (data.startRoomSet == false) {
-            var alertBox = document.getElementById("alert");
-            alertBox.classList.add("alert-danger");
-            alertBox.innerText = "The map file for the start room has not been set so far!";
-            addMapParagraph.innerText = "Set start room URL";
-
-            mapUrlInput.value = data.startRoom.split("/")[4];
-            mapUrlInput.disabled = true;
-        } else {
-            addMapParagraph.innerText = "Add a new room";
-            mapUrlInput.disabled = false;
-            mapUrlInput.value = "";
-            document.getElementById("mapURLFile").value = "";
-        }
-
-        if (data.maps.length > 0) {
-            // show maps
-
-            // add heading
-            var heading = document.createElement('p');
-            heading.classList.add("fs-3");
-            heading.textContent = "Enumeration of existing rooms";
-            contentBox.appendChild(heading);
-
-            // add maps table
-            var table = document.createElement('table');
-            table.classList.add("table");
-            var tableHead = document.createElement('tr');
-
-            var mapUrlColumn = document.createElement('th');
-            mapUrlColumn.textContent = "Map URL";
-
-            var mapFileUrl = document.createElement('th');
-            mapFileUrl.textContent = "Map File URL";
-
-            var mapAccessRestriction = document.createElement('th');
-            mapAccessRestriction.textContent = "Access restriction";
-
-            var mapActions = document.createElement('th');
-            mapActions.textContent = "Actions";
-
-            tableHead.appendChild(mapUrlColumn);
-            tableHead.appendChild(mapFileUrl);
-            tableHead.appendChild(mapAccessRestriction);
-            tableHead.appendChild(mapActions);
-
-            var tableHeadDivider = document.createElement('thead');
-            tableHeadDivider.appendChild(tableHead);
-
-            table.appendChild(tableHeadDivider);
-
-            var tableBody = document.createElement('tbody');
-
-            // add maps to table
-            data.maps.forEach(element => {
-                var tableRow = document.createElement('tr');
-
-                // add domain
-                var tableDataDomain = document.createElement('td');
-                var tableDomainParagraph = document.createElement('p');
-                tableDomainParagraph.classList.add("fw-normal");
-                tableDomainParagraph.textContent = element.url;
-                tableDataDomain.appendChild(tableDomainParagraph);
-                tableRow.appendChild(tableDataDomain);
-
-                // add file url
-                var tableDataFile = document.createElement('td');
-                var tableDataFileParagraph = document.createElement('p');
-                tableDataFileParagraph.classList.add("fw-normal");
-                tableDataFileParagraph.textContent = element.file;
-                tableDataFile.appendChild(tableDataFileParagraph);
-                tableRow.appendChild(tableDataFile);
-
-                // add access restriction
-                var tableAccessRestriction = document.createElement('td');
-                var tableAccessRestrictionParagraph = document.createElement('p');
-                tableAccessRestrictionParagraph.classList.add("fw-normal");
-                if (element.policy == 1) {
-                    tableAccessRestrictionParagraph.textContent = "Public";
-                } else if (element.policy == 2) {
-                    tableAccessRestrictionParagraph.textContent = "Members";
-                } else if (element.policy == 3) {
-                    tableAccessRestrictionParagraph.textContent = "Members with tags";
-
-                    var tags = element.tags;
-                    tags.forEach(tag => {
-                        var tagPill = document.createElement('div');
-                        tagPill.classList.add("badge", "rounded-pill", "bg-primary", "tag");
-                        tagPill.textContent = tag;
-                        tableAccessRestrictionParagraph.appendChild(tagPill);
-                    });
-                }
-                tableAccessRestriction.appendChild(tableAccessRestrictionParagraph);
-                tableRow.appendChild(tableAccessRestriction);
-
-
-                // add remove action
-                var tableRemoveAction = document.createElement('td');
-                var tableRemoveActionButton = document.createElement('button');
-                tableRemoveActionButton.classList.add("tag", "btn", "btn-danger");
-                tableRemoveActionButton.textContent = "Remove";
-                tableRemoveActionButton.addEventListener("click", function () { removeMap(element.url); });
-
-                tableRemoveAction.appendChild(tableRemoveActionButton);
-                tableRow.appendChild(tableRemoveAction);
-
-                tableBody.appendChild(tableRow);
-            });
-
-            // append table
-            table.appendChild(tableBody);
-            contentBox.append(table);
-
-            // select default map access restriction
-            document.getElementById("radioMembers").click();
-            $("#maps").show(750, "swing");
-        }
-    });
+function load() {
+    $("#maps").load("snippets/rooms.php");
+    tagsElementsShown = false;
 }
 
-function removeMap(url) {
-    $.get("ajax_rooms.php?removeMap=" + url, function (result, status) {
-        // check if removal was successful
-        if ("deleteSuccess" in result) {
-            var alertBox = document.getElementById("alert");
-            alertBox.classList.remove("alert-danger");
-            alertBox.classList.remove("alert-success");
-            if (result.deleteSuccess == true) {
-                alertBox.classList.add("alert-success");
-                alertBox.innerText = "The map " + result.mapToRemove.split("/")[4] + " has been removed."
-            } else {
-                alertBox.classList.add("alert-danger");
-                alertBox.innerText = "The map " + result.mapToRemove.split("/")[4] + " could not be removed."
-            }
-        }
-    });
-    loadMaps();
+function remove(mapUrl) {
+    $("#maps").load("snippets/rooms.php?removeMap=" + encodeURI(mapUrl));
+    tagsElementsShown = false;
 }
 
 function addMap() {
@@ -170,7 +27,7 @@ function addMap() {
         }
     }
 
-    var ajaxUrl = "ajax_rooms.php?addMap=" + encodeURI(mapUrl) +
+    var ajaxUrl = "snippets/rooms.php?addMap=" + encodeURI(mapUrl) +
         "&fileUrl=" + encodeURI(mapFileUrl) +
         "&access=" + encodeURI(accessRestriction);
     if (accessRestriction == 3) {
@@ -180,21 +37,8 @@ function addMap() {
         newMapsTags = [];
     }
 
-    $.get(ajaxUrl, function (data, status) {
-        var alertBox = document.getElementById("alert");
-        alertBox.classList.remove("alert-danger");
-        alertBox.classList.remove("alert-success");
-        if (data.storeSuccess) {
-            alertBox.classList.add("alert-success");
-            alertBox.innerText = "Successfully stored map " + mapUrl;
-        } else {
-            alertBox.classList.add("alert-danger");
-            console.log("error: " + data.errorMessage)
-            alertBox.innerText = "Could not store map " + mapUrl + ": " + data.errorMessage;
-        }
-    });
-
-    loadMaps();
+    $("#maps").load(ajaxUrl);
+    tagsElementsShown = false;
 }
 
 function addTagsElements() {
--- website_src/rooms.php
+++ website_src/rooms.php
@@ -42,46 +42,9 @@ session_start();
 
   ?>
 
-  <!-- ajax -->
-  <aside id="alert" class="container alert" role="alert">
-  </aside>
-
   <main class="container">
-
     <!-- ajax -->
     <article id="maps">
-    </article>
-
-    <article>
-      <form action="javascript:void(0)">
-        <p class="fs-3" id="add-map-paragraph">Add a new room</p>
-        <label for="mapURL" class="form-label">Map URL</label>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="map_url_prefix"><?php echo "https://" . getenv('DOMAIN') . "/@/org/" . getenv('DOMAIN') . "/"; ?></span>
-          <input type="text" class="form-control" id="mapURL" aria-describedby="map_url_prefix" name="map_url">
-        </div>
-        <label for="mapURLFile" class="form-label">Map File URL</label>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="map_url_file_prefix">https://</span>
-          <input type="text" class="form-control" id="mapURLFile" aria-describedby="map_url_file_prefix" name="map_file_url">
-        </div>
-        <p class="fs-6">Access restriction</p>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="radio" id="radioPublic" value="public" />
-          <label class="form-check-label" for="radioPublic">Public</label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="radio" id="radioMembers" value="members" checked />
-          <label class="form-check-label" for="radioMembers">Members</label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="radio" id="radioMembersTags" value="tags" />
-          <label class="form-check-label" for="radioMembersTags">Members with tags</label>
-        </div>
-        <div id="tagsArea" class="input-group mb-3">
-        </div>
-        <button class="btn btn-primary" id="addMapButton" style="margin-top: 1rem;">Add map</button>
-      </form>
     </article>
   </main>
 </body>
